224717092b46a0d8803d8e90bb0bf1ee7408bce9
Jakub Radwan <kubeinsztain@gmail.com>
2026-01-10 23:23:36 +0100

    added mapping for moving lines up/down

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -12,3 +12,13 @@ vim.lsp.enable 'gleam'
 
 -- Relative line numbers
 vim.opt.relativenumber = true
+
+-- Move lines up/down with Alt + j / k (VS Code style)
+
+-- Normal mode
+vim.keymap.set('n', '<M-j>', ':m .+1<CR>==', { desc = 'Move line down' })
+vim.keymap.set('n', '<M-k>', ':m .-2<CR>==', { desc = 'Move line up' })
+
+-- Visual mode (move selected block)
+vim.keymap.set('v', '<M-j>', ":m '>+1<CR>gv=gv", { desc = 'Move selection down' })
+vim.keymap.set('v', '<M-k>', ":m '<-2<CR>gv=gv", { desc = 'Move selection up' })
