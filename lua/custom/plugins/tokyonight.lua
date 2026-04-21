37a4c6e2f26b236ff7918d05749832e07fca4537
Jakub Radwan <kubeinsztain@gmail.com>
2026-04-21 08:17:58 +0200

    color override for flash in the tokyonight plugin config

--- lua/custom/plugins/tokyonight.lua
+++ lua/custom/plugins/tokyonight.lua
@@ -5,7 +5,14 @@ return {
     name = 'tokyonight',
     lazy = false, -- load immediately so it shows in "loaded" section
     priority = 1000, -- ensure it's set before other colors
-    opts = {},
+    opts = {
+      style = 'night',
+      on_highlights = function(hl, c)
+        hl.FlashMatch = { bg = c.bg_highlight, fg = c.blue }
+        hl.FlashCurrent = { bg = c.blue, fg = c.bg, bold = true }
+        hl.FlashLabel = { bg = c.magenta, fg = c.bg, bold = true }
+      end,
+    },
     config = function(_, opts)
       require('tokyonight').setup(opts)
       vim.cmd.colorscheme 'tokyonight-night'
